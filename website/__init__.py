2f67e13c04b1521de2ea995124d04b6fef397e28
Derek <dy0291@gmail.com>
2021-10-28 19:06:38 -0400

    Created the database

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
-import os
+from os import path
+
+from flask_sqlalchemy import SQLalchemy
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
+
+db = SQLalchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
 
     # Creating secret key for app to encrypt and secure the cookies.
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    # Setting SQLAchemy database location 
+    app.config['SQLACHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # Taking the database and telling the app that we are using this database
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -17,5 +28,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
     
     return app
+
+def create_database(app):
+    if not path.exist('website/' + DB_NAME):
+        db.create_all(app=app)
---
+++ website/models.py
@@ -0,0 +1,20 @@
+# From the current package import the database
+from enum import unique
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) # func gets the current date
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+
+
+class Person(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True) # Primary Key
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
