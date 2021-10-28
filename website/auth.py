2655376d257eaa4de125d89b920d78b529addda6
Derek <dy0291@gmail.com>
2021-10-27 20:42:42 -0400

    Created all template pages and starting on creating routes

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,18 @@
 # This file is the blueprint of our application
 # It contains authentication routes/URLs for our app
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return 
+    data = request.form.get()
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=["GET", "POST"])
 def signup():
-    return 
+    return render_template("signup.html")
