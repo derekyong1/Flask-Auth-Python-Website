7ea654118aa9784d4b926e263009dfb5c1a52502
Derek <dy0291@gmail.com>
2021-10-28 00:48:03 -0400

    Working on fixing bootstrap components

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 # This file is the blueprint of our application
 # It contains authentication routes/URLs for our app
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -15,4 +15,23 @@ def logout():
 
 @auth.route('/signup', methods=["GET", "POST"])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # Checking if the login credentials are valid
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 6 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+            pass
+        
     return render_template("signup.html")
